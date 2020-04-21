2180a50ecc61c752ac4ab57bcfbbb8c80408d36b
Mark Servilla <mark.servilla@gmail.com>
2020-04-21 16:54:18 -0600

    Add logging; create root directories on startup

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -14,7 +14,13 @@
 """
 import os
 
+import daiquiri
+
 from webapp.config import Config
 
 
-os.makedirs(Config.ROOT_DIR + "/static", exist_ok=True)
+logger = daiquiri.getLogger(__name__)
+
+path = Config.ROOT_DIR + "/static"
+os.makedirs(path, exist_ok=True)
+logger.info(f"Created root working directories: {Config.ROOT_DIR} and {path} ")
