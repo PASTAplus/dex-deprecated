252b2e155c21e2e007ef96dda0e325789bd2082a
Mark Servilla <mark.servilla@gmail.com>
2020-04-21 10:04:30 -0600

    Move plotting functionality to new dplot module

---
+++ webapp/dex/dplot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: dplot
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    4/21/20
+"""
+from bokeh.plotting import figure, output_file, save, show
+from bokeh.models import ColumnDataSource
+
+
+def plot_xy(df, date_x: bool, file_spec: str):
+    output_file(file_spec)
+    if date_x:
+        p = figure(x_axis_type='datetime')
+    else:
+        p = figure()
+    p.xaxis.axis_label = df.keys()[0]
+    p.yaxis.axis_label = df.keys()[1]
+    source = ColumnDataSource(df)
+    p.circle(x=df.keys()[0], y=df.keys()[1], source=source)
+    save(p)
