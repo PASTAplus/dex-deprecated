64bbde6740288c1ea0f0ad901014fca092bf5cfd
Mark Servilla <mark.servilla@gmail.com>
2020-04-23 22:21:16 -0600

    Save dobject data in pickle
    Only create panda dataframe when necessary

--- webapp/dex/dobject.py
+++ webapp/dex/dobject.py
@@ -12,11 +12,11 @@
 :Created:
     4/12/20
 """
-from pathlib import PurePath
-
-import pandas as pd
+from pathlib import Path, PurePath
+import pickle
 
 import daiquiri
+import pandas as pd
 
 
 logger = daiquiri.getLogger(__name__)
@@ -28,15 +28,37 @@ class Dobject:
         self._file_name = entity["file_name"]
         self._file_path = str(PurePath(self._file_spec).parent)
         self._purl = entity["purl"]
-        self._df = pd.read_csv(self._file_spec)
-        self._head = get_head(self._df)
-        self._keys = self._df.keys()
-        self._dtypes = self._df.dtypes.items()
-        self._shape = self._df.shape
-        self._stats = get_stats(self._df)
-        self._rows = self._shape[0]
-        self._cols = self._shape[1]
-        _ = PurePath(self._file_spec)
+        dobject_pkl = self._file_path + "/dobject.pkl"
+        if Path(dobject_pkl).exists():
+            with open(dobject_pkl, "rb") as f:
+                dobject_dict = pickle.load(f)
+            self._head = dobject_dict["head"]
+            self._keys = dobject_dict["keys"]
+            self._dtypes = dobject_dict["dtypes"]
+            self._shape = dobject_dict["shape"]
+            self._stats = dobject_dict["stats"]
+            self._rows = dobject_dict["rows"]
+            self._cols = dobject_dict["cols"]
+        else:
+            self._df = pd.read_csv(self._file_spec)
+            self._head = get_head(self._df)
+            self._keys = self._df.keys()
+            self._dtypes = self._df.dtypes.items()
+            self._shape = self._df.shape
+            self._stats = get_stats(self._df)
+            self._rows = self._shape[0]
+            self._cols = self._shape[1]
+            dobject_dict = {
+                "head": self._head,
+                "keys": self._keys,
+                "dtypes": self._dtypes,
+                "shape": self._shape,
+                "stats": self._stats,
+                "rows": self._rows,
+                "cols": self._cols
+            }
+            with open(dobject_pkl, "wb") as f:
+                pickle.dump(dobject_dict, f)
 
     @property
     def cols(self):
@@ -83,12 +105,14 @@ class Dobject:
         return self._stats
 
     def subset(self, columns: list, r_start: int, r_end: int):
-        df = self._df.iloc[r_start:r_end + 1, columns]
+        df = pd.read_csv(self._file_spec)
+        df = df.iloc[r_start:r_end + 1, columns]
         df.to_csv(self._file_path + "/subset.csv", index=False, header=True)
         return df
 
     def plot(self, columns: list, date_x: bool):
-        df = self._df.iloc[:, columns]
+        df = pd.read_csv(self._file_spec)
+        df = df.iloc[:, columns]
         if date_x:
             df[df.keys()[0]] = pd.to_datetime(df[df.keys()[0]])
         return df
